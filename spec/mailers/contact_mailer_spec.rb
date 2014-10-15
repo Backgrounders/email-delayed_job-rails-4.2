64a8f72059468b30c89bff241df562acd995da64
Derek Maffett <derekgmaffett@gmail.com>
2014-10-14 18:18:45 -0700

    switch over to rspec

--- spec/mailers/contact_mailer_spec.rb
+++ spec/mailers/contact_mailer_spec.rb
@@ -1,7 +1,27 @@
+require 'rails_helper'
+
 describe ContactMailer do
   include EmailSpec::Helpers
   include EmailSpec::Matchers
+
+  before do
+    email = ContactMailer.contact(
+    'The Olsen Twins',
+    'olsentwins@email.com',
+    'Awesome site! We love that this uses a background job.'
+    )
+  end
+
+  it 'should have a reply email' do
+    email.must_have_reply_to 'example@email.com'
+  end
+
   it 'should have a subject' do
-    assert false
+    email.must_have_subject 'Thanks for dropping a message!'
+  end
+
+  it 'should have a body' do
+    email.must_have_body_text "Thanks for contacting us, The Olsen Twins! We'll
+    be sure to get back to you soon."
   end
 end
